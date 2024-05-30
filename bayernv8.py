6c6c47f06a101cf589d3a890111ad98e6cfcfa2f
Gonzal22 <89180863+Gonzal22@users.noreply.github.com>
2024-05-30 16:52:49 -0600

    bayernv8.py

--- bayernv8.py
+++ bayernv8.py
@@ -48,8 +48,23 @@ def mostrar_informacion_trafico():
         for i, num_cars in enumerate(cars):
             st.write(f"*Semáforo {i + 1}*: {num_cars} carros")
 
+        # Nueva gráfica de la suma total de carros
+        total_cars = sum(cars)
+        st.write("### Total de carros en todos los semáforos 🚗:")
+        st.write(f"{total_cars} carros")
+        
+        df_total = pd.DataFrame({
+            'Total de carros': [total_cars]
+        })
+        
+        chart_total = alt.Chart(df_total).mark_bar().encode(
+            x=alt.X('Total de carros', type='ordinal'),
+            y='Total de carros'
+        )
+        
+        st.altair_chart(chart_total, use_container_width=True)
 
-        time.sleep(0.5)
+        time.sleep(0.2)
         st.rerun()
     else:
         st.warning("No se pudo obtener datos del servidor.")
@@ -66,21 +81,21 @@ def abrir_imagen_con_transparencia(path, size):
         return None
 
 def mostrar_control_semaforos():
-    
+    # Rutas de las imágenes (deben estar en el mismo directorio o en subdirectorios)
     base_image_path = "calle.jpeg"
     semaforo_verde_path = "verde3.png"
     semaforo_rojo_path = "rojo4.png"
 
     semaforo_size = (50, 50)  
 
-    base_image = abrir_imagen_con_transparencia(base_image_path,(1000,800))
+    base_image = abrir_imagen_con_transparencia(base_image_path, (1000, 800))
     semaforo_verde = abrir_imagen_con_transparencia(semaforo_verde_path, semaforo_size)
     semaforo_rojo = abrir_imagen_con_transparencia(semaforo_rojo_path, semaforo_size)
 
     if base_image is None or semaforo_verde is None or semaforo_rojo is None:
         return
 
-    semaforo_positions = [ (380, 400),(490, 600),(600, 380),(480, 200)]
+    semaforo_positions = [(380, 400), (490, 600), (600, 380), (480, 200)]
 
     while True:
         datos = obtener_datos_trafico(url)
@@ -97,11 +112,11 @@ def mostrar_control_semaforos():
                     image_with_semaforos.paste(semaforo_rojo, pos, semaforo_rojo)
 
             st.image(image_with_semaforos, use_column_width=True)
-            time.sleep(0.5)
+            time.sleep(0.2)
             st.rerun()
         else:
             st.warning("No se pudo obtener datos del servidor.")
-            time.sleep(0.5)
+            time.sleep(0.2)
             st.rerun()
 
 pagina = st.sidebar.radio("Selecciona una página", ["Información de Tráfico", "Control de Semáforos"])
